90413a636b92e95d0a8e42630ff955c1bb2ecd69
kusumi <kusumi@INkusumi.local>
2015-08-24 18:59:22 +0530

    commit

--- src/Baseline.java
+++ src/Baseline.java
@@ -5,15 +5,23 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Baseline {
+    int[] quantity = new int[20];
+    int[] price = new int[20];
+    String[] item = new String[20];
+
     public static void main(String[] args) {
         Baseline bl = new Baseline();
         bl.readInput();
+        bl.calculateTax();
+    }
+
+    private void calculateTax() {
+
     }
 
     private void readInput() {
         String temp, in;
-        String[] quantity = new String[20];
-        String[] price = new String[20];
+
         int itemCounter=0;
         Scanner s = new Scanner(System.in);
         System.out.println("Enter input item or press 0 to exit");
@@ -24,12 +32,14 @@ public class Baseline {
                 break;
             }
             else{
+                item[itemCounter]=in;
                 quantity[itemCounter]=in.substring(0,1);
                 temp = in.substring(1);
                 temp= temp.replaceAll("\\D+", "");
                 price[itemCounter]= temp;
                 System.out.println("Quantity is " +quantity[itemCounter]);
                 System.out.println("Price is "+ price[itemCounter]);
+                System.out.println("Item is "+ item[itemCounter]);
                 itemCounter++;
             }
         }
