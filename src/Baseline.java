4ec8c6f995d0f7286b7e4141b545df26a9545991
kusumi <kusumi@INkusumi.local>
2015-08-24 18:48:17 +0530

    commit

--- src/Baseline.java
+++ src/Baseline.java
@@ -2,6 +2,8 @@
  * Created by kusumi on 24/08/15.
  */
 import java.io.*;
+import java.util.Scanner;
+
 public class Baseline {
     public static void main(String[] args) {
         Baseline bl = new Baseline();
@@ -10,10 +12,23 @@ public class Baseline {
 
     private void readInput() {
         String in;
+        String[] quantity = new String[20];
+        String[] price = new String[20];
+        int itemCounter=0;
         Scanner s = new Scanner(System.in);
         System.out.println("Enter input item or press 0 to exit");
-        while(!(s.nextLine().equals("0"))){
-            in = s.nextLine();
+        while(true){
+            in=s.nextLine();
+            if(in.equals("0")){
+                System.out.println("exiting");
+                break;
+            }
+            else{
+                quantity[itemCounter]=in.substring(0,1);
+
+                System.out.println(quantity[itemCounter]);
+            }
         }
+        //System.out.println("Input String is " + total);
     }
 }
